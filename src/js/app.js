6664cb0de93c60253e07fb626e6ef45a1949377e
coursar <coursar@gmail.com>
2023-02-27 10:40:58 +0300

    activate file chooser by js

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,21 @@
 const rootEl = document.querySelector('#root');
 rootEl.innerHTML = `
-<a data-id="download" href="http://localhost:9999">Download file</a>
-<img data-id="preview">
-<a data-id="ghost" class="hidden"></a>
+<button data-id="upload">Upload File</button>
+
+<div class="file-selector">
+  <input data-id="file" type="file">
+  <span class="hider"></span>
+</div>
 `;
 
-const imgEl = rootEl.querySelector('[data-id="preview"]');
+const fileEl = rootEl.querySelector('[data-id="file"]');
 
-const linkEl = rootEl.querySelector('[data-id="download"]');
-const ghostEl = rootEl.querySelector('[data-id="ghost"]');
-
-linkEl.addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://localhost:9999');
-  xhr.responseType = 'blob'; // fetch.blob()
-  xhr.send();
-
-  xhr.onload = (evt) => {
-    const url = URL.createObjectURL(evt.target.response); // blob
-    ghostEl.href = url;
-    ghostEl.download = 'image.png';
-    // v1
-    // ghostEl.click();
-    // v2
-    ghostEl.dispatchEvent(new PointerEvent('click', {}));
-
-    setTimeout(() => {
-      URL.revokeObjectURL(url);
-    }, 60000);
-  };
+const buttonEl = rootEl.querySelector('[data-id="upload"]');
+buttonEl.addEventListener('click', (evt) => {
+  // 1. user activation
+  fileEl.click();
 });
 
-ghostEl.addEventListener('click', (evt) => {
+fileEl.addEventListener('change', (evt) => {
+  debugger;
 });
