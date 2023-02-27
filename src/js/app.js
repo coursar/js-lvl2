322e696f7e5780890ef8c7a1900f7f65be87bfc9
coursar <coursar@gmail.com>
2023-02-27 09:43:45 +0300

    image display

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,28 @@
 const rootEl = document.querySelector('#root');
 rootEl.innerHTML = `
-<a href="http://localhost:9999">Download file</a>
+<a data-id="download" href="http://localhost:9999">Download file</a>
+<img data-id="preview">
+<a data-id="ghost" class="hidden"></a>
 `;
+
+const imgEl = rootEl.querySelector('[data-id="preview"]');
+
+const linkEl = rootEl.querySelector('[data-id="download"]');
+linkEl.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://localhost:9999');
+  xhr.responseType = 'blob'; // fetch.blob()
+  xhr.send();
+
+  xhr.onload = (evt) => {
+    const url = URL.createObjectURL(evt.target.response); // blob
+    imgEl.src = url;
+    imgEl.onload = (evt) => {
+      URL.revokeObjectURL(url);
+    };
+    debugger;
+  };
+
+});
